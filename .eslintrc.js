83ae2981b7b016f5a63762937d69fba1e5dcb6ee
Joel Mello JS <mellojoel.hr@gmail.com>
2025-12-02 17:59:07 +0000

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks", "prettier"],
+  rules: {
+    // aqui você pode adicionar regras personalizadas
+    "prettier/prettier": "error",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
+
+export default config;
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true, // Sempre adicionar ponto e vírgula ao final das linhas.
+  trailingComma: "es5", // Adiciona vírgula no final de objetos e arrays (apenas onde é válido em ES5, por exemplo em listas multiline).
+  singleQuote: true, // Usa aspas simples ('') em vez de aspas duplas ("") para strings.
+  printWidth: 80, // Limita o comprimento da linha a 80 caracteres antes de quebrar para a próxima linha.
+  tabWidth: 2, // Define que cada indentação equivale a 2 espaços.
+  useTabs: false, // Usa espaços para identação em vez de tabulações.
+};
